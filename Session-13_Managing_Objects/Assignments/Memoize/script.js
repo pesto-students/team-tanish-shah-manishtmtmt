cacf1b99afb3cfe759b88ad710dabce992b102b6
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-06-20 09:52:21 +0530

    session-13 memoize solution modify

--- Session-13_Managing_Objects/Assignments/Memoize/script.js
+++ Session-13_Managing_Objects/Assignments/Memoize/script.js
@@ -11,7 +11,7 @@ function memoize(fn) {
       cache[key] = result;
       return result;
     }
-  }
+  };
 }
 
 // Use hard coded data
@@ -23,7 +23,8 @@ const temperatureData = {
   Sydney: 28,
 };
 
-// create a function that will return the temperature of the given city
+/*
+
 function getTemperatureForCity(city) {
   return temperatureData[city];
 }
@@ -31,4 +32,27 @@ function getTemperatureForCity(city) {
 // Memoize the getTemperatureForCity function
 const getWeatherForCity = memoize(getTemperatureForCity);
 
+*/
+
+// create a function that will return the temperature of the given city
+function getTemperatureForCity() {
+  const cache = {};
+
+  return (city) => {
+    if (cache[city]) {
+      return cache[city];
+    } else {
+      const result = temperatureData[city];
+      cache[city] = result;
+      return result;
+    }
+  };
+}
+
+// Memoize the getTemperatureForCity function
+const getWeatherForCity = getTemperatureForCity();
+
+console.log(getWeatherForCity("New York")); // 20
+console.log(getWeatherForCity("London")); // 18
+console.log(getWeatherForCity("Paris")); // 22
 console.log(getWeatherForCity("New York")); // 20
