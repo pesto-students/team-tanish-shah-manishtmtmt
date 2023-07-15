aaf284c3f2cba91cf32ac1714014d04c6bad2275
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-07-15 18:14:53 +0530

    dsa week-01 arrays-1 and arrays-2 assignments solutions

---
+++ DSA-Assignments/week 1/Arrays 1/pairOfSum.js
@@ -0,0 +1,20 @@
+const pairOfSum = (arr, S) => {
+  const pairs = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      const sum = arr[i] + arr[j];
+      if (sum === S) pairs.push([arr[i], arr[j]]);
+    }
+  };
+
+  return pairs;
+}
+
+const arr1 = [2, 4, 6, 8, 10];
+const S1 = 12;
+console.log(pairOfSum(arr1, S1)); // [ [ 2, 10 ], [ 4, 8 ] ]
+
+const arr2 = [1, 1, 1, 1, 1];
+const S2 = 2;
+console.log(pairOfSum(arr2, S2)); // [ [ 1, 1 ], [ 1, 1 ], [ 1, 1 ], [ 1, 1 ], [ 1, 1 ], [ 1, 1 ], [ 1, 1 ], [ 1, 1 ], [ 1, 1 ], [ 1, 1 ] ]
